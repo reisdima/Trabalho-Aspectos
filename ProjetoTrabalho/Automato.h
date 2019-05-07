5a3fd23a0358fb03b620bcd250a3968e3d2a8219
pedrocotta98 <37852363+pedrocotta98@users.noreply.github.com>
2019-05-07 14:45:23 -0300

    Add files via upload
    ADD CLASSE AUTOMATO E TRANSICAO

---
+++ ProjetoTrabalho/Automato.h
@@ -0,0 +1,20 @@
+#ifndef AUTOMATO_H
+#define AUTOMATO_H
+
+
+class Automato
+{
+    public:
+        Automato();
+        ~Automato();
+
+
+
+    private:
+        string alfabeto[];
+        string conjuntoEstados[];
+        string estadoInicial[];
+
+};
+
+#endif // AUTOMATO_H
